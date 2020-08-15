267488cc7028ed8888b9c8af21c8c418e071edb2
AmossGuy <yellowilya@gmail.com>
2020-08-15 12:00:56 -0400

    Commit

--- objects/ball.gd
+++ objects/ball.gd
@@ -1,6 +1,18 @@
 extends RigidBody2D
 
+var on_floor = false
+
+func update_arrows(count: int, dir: float):
+	var arrows := [$arrow1, $arrow2, $arrow3]
+
 func _input(event):
-	if event.is_action_pressed("launch"):
+	if event.is_action_pressed("launch") and on_floor:
 		var mouse_vec = get_local_mouse_position().rotated(rotation)
-		apply_central_impulse(mouse_vec)
+		apply_central_impulse(mouse_vec.normalized() * 100)
+
+func _integrate_forces(s):
+	on_floor = false
+	for x in range(s.get_contact_count()):
+		var ci = s.get_contact_local_normal(x)
+		if ci.dot(Vector2(0, -1)) > 0.6:
+			on_floor = true
