335748a7cf3bc94e92ec9f9373af6dcf66e7d6b8
AmossGuy <yellowilya@gmail.com>
2020-08-15 16:57:41 -0400

    rigid ball → kinematic house

--- objects/ball.gd
+++ objects/ball.gd
@@ -1,13 +1,41 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-class_name ball
-func get_class():
-	return "ball"
-func is_class(name: String):
-   return .is_class(name) or (get_class() == name)
+const GRAVITY := 98
+const FRICTION := 170
+const ARROW_OFFSET := 16
+const LAUNCH_FORCE := 100
 
-var on_floor := false
-var special_launch := false
+var velocity := Vector2.ZERO
+
+func _process(delta: float):
+	velocity += Vector2.DOWN * GRAVITY * delta
+	if is_on_floor():
+		if velocity.x > 0:
+			velocity.x -= FRICTION * delta
+			velocity.x = max(velocity.x, 0)
+		elif velocity.x < 0:
+			velocity.x += FRICTION * delta
+			velocity.x = min(velocity.x, 0)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	var launch_vec := get_launch_vec()
+	update_arrows(1 if launch_vec != Vector2.INF else 0, launch_vec.angle())
+
+func _input(event):
+	if event.is_action_pressed("launch") and is_on_floor():
+		var launch_vec := get_launch_vec()
+		if launch_vec != Vector2.INF:
+			velocity = launch_vec
+
+func test_launch(launch_vec: Vector2) -> Vector2:
+	var collision := move_and_collide(launch_vec, true, true, true)
+	return launch_vec if collision == null else Vector2.INF
+
+func get_launch_vec() -> Vector2:
+	var launch_vec := get_local_mouse_position().rotated(rotation) \
+		.normalized() * LAUNCH_FORCE
+	launch_vec = test_launch(launch_vec)
+	return launch_vec
 
 func update_arrows(count: int, angle: float):
 	var arrows := [$arrow1, $arrow2, $arrow3]
@@ -17,28 +45,4 @@ func update_arrows(count: int, angle: float):
 		arrows[i].global_rotation = angle
 		arrows[i].global_position = global_position \
 			+ get_local_mouse_position().rotated(rotation) \
-			.normalized() * 16 * (i+1)
-
-func _process(delta):
-	update_arrows(
-		1 if (on_floor or special_launch) else 0,
-		get_local_mouse_position().rotated(rotation).angle()
-	)
-
-func _input(event):
-	if event.is_action_pressed("launch") and (on_floor or special_launch):
-		special_launch = false
-		var mouse_vec = get_local_mouse_position().rotated(rotation)
-		linear_velocity = linear_velocity \
-			.rotated(-mouse_vec.angle())
-		linear_velocity.y = 0
-		linear_velocity = linear_velocity \
-			.rotated(mouse_vec.angle())
-		apply_central_impulse(mouse_vec.normalized() * 100)
-
-func _integrate_forces(s):
-	on_floor = false
-	for x in range(s.get_contact_count()):
-		var ci = s.get_contact_local_normal(x)
-		if ci.dot(Vector2(0, -1)) > 0.6:
-			on_floor = true
+			.normalized() * ARROW_OFFSET * (i+1)
