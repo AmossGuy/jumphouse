50c664482d294d3c810e0279ed5d20ed75dbab55
AmossGuy <yellowilya@gmail.com>
2020-08-16 18:19:07 -0400

    add splash sound

--- objects/kill_area.gd
+++ objects/kill_area.gd
@@ -4,5 +4,8 @@ export(NodePath) var destination
 
 func _on_kill_area_body_entered(body):
 	if body.is_class("ball"):
+		$splash_sound.global_position = body.global_position
+		$splash_sound.play()
+		
 		body.velocity = Vector2.ZERO
 		body.global_position = get_node(destination).global_position
