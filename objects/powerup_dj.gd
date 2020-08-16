579d6d110f6ad33f1f3022439e624bed497773a3
AmossGuy <yellowilya@gmail.com>
2020-08-16 11:31:54 -0400

    fix double jump powerup

--- objects/powerup_dj.gd
+++ objects/powerup_dj.gd
@@ -3,11 +3,12 @@ extends Area2D
 func _on_powerup_dj_body_entered(body):
 	if body.is_class("ball"):
 		body.has_double_jump = true
+		body.double_jump_charged = true
 		$shatter_sound.play()
 		
 		visible = false
-		monitoring = false
-		monitorable = false
+		set_deferred("monitoring", false)
+		set_deferred("monitorable", false)
 
 func _on_shatter_sound_finished():
 	queue_free()
