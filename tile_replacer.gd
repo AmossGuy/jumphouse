2b23453ebbe276973cf32493fb8fff557bae0ecb
AmossGuy <yellowilya@gmail.com>
2020-08-16 13:53:18 -0400

    fix replacer script

--- tile_replacer.gd
+++ tile_replacer.gd
@@ -3,13 +3,7 @@ extends TileMap
 var rng = RandomNumberGenerator.new()
 
 func _ready():
-	var placeholder := 40 + 2 * 48
-	var buildings := [0 + 21 * 48, 1 + 21 * 48, 3 + 21 * 48, 4 + 21 * 48]
-	
-	for cell in get_used_cells():
-		print(get_cellv(cell))
-	
-	for cell in get_used_cells_by_id(placeholder):
-		set_cellv(cell, rng.randi_range(0, buildings.size()-1))
+	for cell in get_used_cells_by_id(6):
+		set_cellv(cell, rng.randi_range(7, 10))
 	
 	update_dirty_quadrants()
