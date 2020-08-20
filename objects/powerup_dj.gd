38f9a9f2284b4438c961b8afba2baf0936b82608
AmossGuy <yellowilya@gmail.com>
2020-08-19 21:07:51 -0400

    add finale

--- objects/powerup_dj.gd
+++ objects/powerup_dj.gd
@@ -1,5 +1,22 @@
 extends Area2D
 
+export var respawns := true
+
+var timer := 0.0
+var timer_active := false
+
+func _process(delta):
+	if timer_active:
+		timer += delta
+		if timer >= 3:
+			$respawn_sound.play()
+			
+			visible = true
+			set_deferred("monitoring", true)
+			set_deferred("monitorable", true)
+			
+			timer_active = false
+
 func _on_powerup_dj_body_entered(body):
 	if body.is_class("ball"):
 		body.has_double_jump = true
@@ -9,6 +26,10 @@ func _on_powerup_dj_body_entered(body):
 		visible = false
 		set_deferred("monitoring", false)
 		set_deferred("monitorable", false)
+		
+		timer_active = true
+		timer = 0.0
 
 func _on_shatter_sound_finished():
-	queue_free()
+	if not respawns:
+		queue_free()
